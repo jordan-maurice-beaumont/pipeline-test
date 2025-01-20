877ad3c4f4e88fbab29068a523c90e1763226c1a
jordan-maurice-beaumont <jordan.beaumont@bbd.co.za>
2025-01-20 12:21:54 +0000

    added breaking test to test pipeline

--- spec/tester.spec.js
+++ spec/tester.spec.js
@@ -2,5 +2,8 @@ describe("A suite", function () {
     it("contains a spec with an expectation", function () {
       expect(true).toBe(true);
     });
+    it("contains a spec with an expectation", function () {
+      expect(false).toBe(true);
+    });
   });
   
